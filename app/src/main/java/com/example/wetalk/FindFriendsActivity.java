945a65f2aa1bd2068be07d61f9f0fb2ad2029f05
Idan <idanmosh21@gmail.com>
2019-12-19 16:58:04 +0200

    new commit

--- app/src/main/java/com/example/wetalk/FindFriendsActivity.java
+++ app/src/main/java/com/example/wetalk/FindFriendsActivity.java
@@ -1,8 +1,8 @@
 package com.example.wetalk;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FindFriendsActivity extends AppCompatActivity {
 
