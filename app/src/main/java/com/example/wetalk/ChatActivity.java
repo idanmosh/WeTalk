66c31e57721a8bdf40cb05723c093735e83ff2e3
Idan <idanmosh21@gmail.com>
2020-03-19 14:25:28 +0200

    finished sync contacts last time

--- app/src/main/java/com/example/wetalk/ChatActivity.java
+++ app/src/main/java/com/example/wetalk/ChatActivity.java
@@ -7,13 +7,11 @@ import android.provider.ContactsContract;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.wetalk.Classes.Contact;
+
 public class ChatActivity extends AppCompatActivity {
 
-    private String id;
-    private String phone;
-    private String name;
-    private String image;
-    private String status;
+    private Contact mContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +19,30 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         initContactData();
+
+
     }
 
     private void initContactData() {
         Uri intentData = getIntent().getData();
-        if (!Uri.EMPTY.equals(intentData)) {
-            assert intentData != null;
+        if (intentData != null) {
             Cursor c = getContentResolver().query(intentData, null, null, null, null);
             assert c != null;
             if (c.moveToNext()) {
-                id = c.getString(c.getColumnIndex(ContactsContract.Data.DATA7));
-                phone = c.getString(c.getColumnIndex(ContactsContract.Data.DATA1));
-                name = c.getString(c.getColumnIndex(ContactsContract.Data.DATA2));
-                image = c.getString(c.getColumnIndex(ContactsContract.Data.DATA5));
-                status = c.getString(c.getColumnIndex(ContactsContract.Data.DATA6));
+                String id = c.getString(c.getColumnIndex(ContactsContract.Data.DATA7));
+                String phone = c.getString(c.getColumnIndex(ContactsContract.Data.DATA1));
+                String name = c.getString(c.getColumnIndex(ContactsContract.Data.DATA2));
+                String userId = c.getString(c.getColumnIndex(ContactsContract.Data.DATA4));
+                String image = c.getString(c.getColumnIndex(ContactsContract.Data.DATA5));
+                String status = c.getString(c.getColumnIndex(ContactsContract.Data.DATA6));
+
+                mContact = new Contact(userId,id,name,phone,status,image);
             }
 
             c.close();
         }
+        else
+            mContact = (Contact) getIntent().getSerializableExtra("CONTACT");
+
     }
 }
